bfc397dfea3a61bd632a2b87fe9ef6c14a573126
John <jarrington08@gmail.com>
2023-07-19 20:48:16 -0500

    solved the sorting problem, the page loads correctly

--- script.js
+++ script.js
@@ -3,30 +3,38 @@ window.addEventListener("load", function() {
     fetchPromise.then( function(response) {
         const jsonPromise = response.json();
         jsonPromise.then( function(json) {
+            let numHours = [];
             let astronautCount = 0;
-            //TODO
-            //To sort by num hours, iterate through array once and extract num hours and id. sort greatest to least
-            //iterate through astronaut array again and use boolean logic to go through the array of hours in correct order
-            const div = document.getElementById("container");
             for (let i = 0; i < json.length; i++) {
-            astronautCount++;
-            let activeTrue = '';
-            if (json[i].active === true) {
-                activeTrue = 'style="color:green"';
+                numHours.push(json[i].hoursInSpace);
             }
-            div.innerHTML += `
-            <div class="astronaut">
-                <div class="bio">
-                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
-                    <ul>
-                        <li>Hours in space: ${json[i].hoursInSpace}</li>
-                        <li ${activeTrue}>Active: ${json[i].active}</li>
-                        <li>Skills: ${json[i].skills.join(', ')}</li>
-                    </ul>
-                </div>
-                <img class="avatar" src="${json[i].picture}">
-            </div>`;
-            }
+            numHours = numHours.sort(function(a,b){return b-a});
+            console.log(numHours);
+            const div = document.getElementById("container");
+            for (let i = 0; i < numHours.length; i++) {
+                for (let j = 0; j < json.length; j++) {
+                    if (json[j].hoursInSpace === numHours[i]) {
+                        astronautCount++;
+                        let activeTrue = '';
+                        if (json[j].active === true) {
+                            activeTrue = 'style="color:green"';
+                        }
+                        div.innerHTML += `
+                        <div class="astronaut">
+                            <div class="bio">
+                                <h3>${json[j].firstName} ${json[j].lastName}</h3>
+                                <ul>
+                                    <li>Hours in space: ${json[j].hoursInSpace}</li>
+                                    <li ${activeTrue}>Active: ${json[j].active}</li>
+                                    <li>Skills: ${json[j].skills.join(', ')}</li>
+                                </ul>
+                            </div>
+                            <img class="avatar" src="${json[j].picture}">
+                        </div>`;
+                    }
+                
+                }
+            } 
             document.getElementById("count").innerHTML = `Total Number of Astronauts: ${astronautCount}`;
         });
     });
